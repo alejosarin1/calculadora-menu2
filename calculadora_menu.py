f97247199682d6a0fad6918cc5952826b3826099
alejosarin1 <alejitosarin@gmail.com>
2022-09-10 08:47:29 -0500

    codigo actualisado

--- calculadora_menu.py
+++ calculadora_menu.py
@@ -8,7 +8,7 @@ from math import log
 print("------------------------------------")
 print("--------Calculadora - Menu----------")
 print("------------------------------------")
-
+bandera=False
 x= float(input("dame el valor del numero x :"))
 y= float(input("Dame el valor del numero y :"))
 
@@ -21,4 +21,36 @@ print("4. Dividir.(El primero sobre  el segundo)")
 print("5. Potencia(El primero a la potencia del segundo)")
 print("6. Logaritmo(El Logari del primero)")
 
-opcion = input("\nDame la opcion")
+opcion = int(input("\nDame la opcion : "))
+
+#procesing
+if (opcion== 1):
+    z= x+y
+    print(x,"+",y)
+elif (opcion== 2):
+    z=x-y
+    print(x,"-",y)
+elif(opcion== 3):
+    z=x*y
+    print(x,"*",y)
+elif(opcion==4):
+    z=x/4
+    print(x,"/",y)
+elif(opcion==4 and y==0):
+    print("El denomidador es igual a cero y ")
+    print("No se puede realizar la operacio")
+    bandera=True
+elif(opcion== 5):
+    z=pow(x,y)
+    print(x,"expo",y)
+elif(opcion==6):
+    z=log(x) 
+    print("Loaritmo de ",x)
+elif(opcion== 6 and x<= 0 ):
+    print("No se puede calcular el logaritmo")
+    bandera= True
+else:
+    print("Opccion no valida")
+# se escribe el resultado con otra condicion
+if(opcion< 7 and bandera== False):
+    print("Resultado :",z)
